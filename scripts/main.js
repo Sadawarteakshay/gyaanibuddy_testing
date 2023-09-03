46ae7b5ed2892d8147bf699014ddcc8e6633f179
William Nguyen <williamngoc93@hotmail.com>
2023-09-03 11:03:12 -0700

    basic selenium script

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+const {
+	Builder,
+	By,
+	Key,
+	until
+} = require('selenium-webdriver');
+
+(async function example() {
+	let driver = await new Builder().forBrowser("chrome").build();
+	try {
+		await driver.get('https://www.google.com/');
+		await driver.findElement(By.xpath("//textarea[@id='APjFqb']")).sendKeys('bradley cooper venice film festival', Key.RETURN);
+		await driver.wait(until.titleIs('bradley cooper venice film festival'), 5000);
+	} finally {
+		await driver.quit();
+	}
+})();
