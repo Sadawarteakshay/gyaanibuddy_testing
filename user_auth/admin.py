d0a18ea8940f6d4029b2e595058c3d1d8baebc9d
Akshay Sadawarte <50912349+Sadawarteakshay@users.noreply.github.com>
2023-09-10 11:12:11 -0700

    Delete user_auth directory

--- user_auth/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import User
-# Register your models here.
-
-
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    filter_vertical = ('followers', 'following',)
