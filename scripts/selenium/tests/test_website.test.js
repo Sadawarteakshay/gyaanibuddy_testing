7eaebe9896dbe12b92171ab10a81e192ea8ca29c
William Nguyen <williamngoc93@hotmail.com>
2023-09-18 19:41:13 -0700

    Search bar example for gyaanibuddy

--- scripts/selenium/tests/test_website.test.js
+++ scripts/selenium/tests/test_website.test.js
@@ -2,9 +2,12 @@ const { Builder, By, Key, until } = require('selenium-webdriver');
 
 beforeAll(async () => {
     driver = await new Builder().forBrowser('chrome').build()
+    await driver.manage().window().setRect({ width: 1280, height: 1024 });
 })
   
 afterAll(async () => driver.quit())
+
+let long_timeout = 20000;
 
 test('social media links', async () => {
     await driver.get('https://www.gyaanibuddy.com/');
@@ -12,14 +15,30 @@ test('social media links', async () => {
     
     await home_button.click();
 
-	await driver.wait(until.titleIs('Gyaani Buddy - YouTube'), 5000);
+	await driver.wait(until.titleIs('Gyaani Buddy - YouTube'), long_timeout);
 });
 
-test('social media links 2', async () => {
-    await driver.get('https://www.gyaanibuddy.com/');
-	home_button = await driver.findElement(By.xpath("//a[@class='youtube']"));
+describe("test search button in blogs", () => {
     
-    await home_button.click();
+    test("go to website", async () => {
+        await driver.get('https://www.gyaanibuddy.com/');
+        await driver.wait(until.titleContains('GyaaniBuddy'), long_timeout);
+    });
 
-	await driver.wait(until.titleIs('Gyaani Buddy - YouTube'), 5000);
+    test("click on blogs", async () => {
+        blog_button = await driver.findElement(By.xpath("(//a[@href='/blog/'])[1]"));
+
+        await blog_button.click();
+        await driver.wait(until.titleContains('blogs'), long_timeout);
+    });
+
+    test("search with 'essential' in search bar", async () => {
+        search_button = await driver.findElement(By.id("id_q"));
+        submit_button = await driver.findElement(By.xpath("//button[@class='submit-buttons m-1 p-2']"))
+
+        await search_button.sendKeys('Essential');
+        await submit_button.click();
+
+        await driver.wait(until.urlContains('Essential'), long_timeout);
+    }, long_timeout);
 });
