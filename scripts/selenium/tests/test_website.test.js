feb255390dd5cbb1462b6d5341566b56b29daca9
William Nguyen <williamngoc93@hotmail.com>
2023-09-24 10:06:24 -0700

    Added linting support for jest and refactored tests by adding change to global timeout

--- scripts/selenium/tests/test_website.test.js
+++ scripts/selenium/tests/test_website.test.js
@@ -1,35 +1,38 @@
 const { Builder, By, Key, until } = require('selenium-webdriver');
 
+let long_timeout = 20000;
+
 beforeAll(async () => {
+    jest.setTimeout(long_timeout);
     driver = await new Builder().forBrowser('chrome').build()
     await driver.manage().window().setRect({ width: 1280, height: 1024 });
-})
+}, long_timeout)
   
 afterAll(async () => driver.quit())
 
-let long_timeout = 20000;
+describe("test social media links", () => {
+    test('social media links', async () => {
+        await driver.get('https://www.gyaanibuddy.com/');
+        home_button = await driver.findElement(By.xpath("//a[@class='youtube']"));
+        
+        await home_button.click();
 
-test('social media links', async () => {
-    await driver.get('https://www.gyaanibuddy.com/');
-	home_button = await driver.findElement(By.xpath("//a[@class='youtube']"));
-    
-    await home_button.click();
-
-	await driver.wait(until.titleIs('Gyaani Buddy - YouTube'), long_timeout);
+        await driver.wait(until.titleIs('Gyaani Buddy - YouTube'));
+    });
 });
 
 describe("test search button in blogs", () => {
     
     test("go to website", async () => {
         await driver.get('https://www.gyaanibuddy.com/');
-        await driver.wait(until.titleContains('GyaaniBuddy'), long_timeout);
+        await driver.wait(until.titleContains('GyaaniBuddy'));
     });
 
     test("click on blogs", async () => {
         blog_button = await driver.findElement(By.xpath("(//a[@href='/blog/'])[1]"));
 
         await blog_button.click();
-        await driver.wait(until.titleContains('blogs'), long_timeout);
+        await driver.wait(until.titleContains('blogs'));
     });
 
     test("search with 'essential' in search bar", async () => {
@@ -39,6 +42,6 @@ describe("test search button in blogs", () => {
         await search_button.sendKeys('Essential');
         await submit_button.click();
 
-        await driver.wait(until.urlContains('Essential'), long_timeout);
-    }, long_timeout);
+        await driver.wait(until.urlContains('Essential'));
+    });
 });
