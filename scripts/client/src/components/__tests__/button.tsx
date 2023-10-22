c136f10480ef2465c3d79669dce879e72e6faeff
William Nguyen <williamngoc93@hotmail.com>
2023-10-22 10:51:01 -0700

    add snapshot testing and some unit tests

--- scripts/client/src/components/__tests__/button.tsx
+++ scripts/client/src/components/__tests__/button.tsx
@@ -1,13 +1,35 @@
 import React from 'react';
 
-import { render, cleanup } from '../../test-utils';
+import { render, cleanup, screen, fireEvent, waitFor } from '../../test-utils';
 import Button from '../button';
+
+import renderer from 'react-test-renderer';
+import { colors } from '../../styles';
 
 describe('Button', () => {
   // automatically unmount and cleanup DOM after the test is finished.
   afterEach(cleanup);
 
   it('renders without error', () => {
-    render(<Button>Hello World</Button>);
+    const HelloWorldButton = <Button>Hello World</Button>;
+
+    render(HelloWorldButton);
+
+    // Snapshot test - ensure button has no unsuspecting changes between commits
+    const ButtonTree = renderer
+      .create(HelloWorldButton)
+      .toJSON();
+
+    expect(ButtonTree).toMatchSnapshot();
   });
+
+  // it('button background color should change color when hovered over', async () => {
+  //   const HelloWorldButton = <Button>Hello World</Button>;
+
+  //   render(HelloWorldButton);
+
+  //   fireEvent.mouseOver(screen.getByText('Hello World'));
+
+  //   expect(screen.)
+  // });
 });
